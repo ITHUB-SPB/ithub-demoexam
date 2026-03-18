f0bd5a92755cb8f6a2b298721abed4f68a80eee3
Lex Trofimov <daslef93@gmail.com>
2026-03-18 18:41:06 +0300

    логин

--- main.py
+++ main.py
@@ -7,10 +7,18 @@ from starlette.requests import Request
 from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 
+from authx import AuthX, AuthXConfig
+
 from db import Course, engine, Payment, Request as UserRequest, User
+
+auth = AuthX(config=AuthXConfig(
+    JWT_SECRET_KEY="jhiasdijhsadjkxjzxcuio321",
+    JWT_TOKEN_LOCATION=["cookies"]
+))
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
 
 @app.post('/create/')
 def create_request(
@@ -27,7 +35,15 @@ def create_request(
     return RedirectResponse('/', status_code=303)
 
 @app.get('/')
-def index(request: Request):
+async def index(request: Request):
+    token = await auth.get_token_from_request(request)
+    payload = auth.verify_token(token, verify_csrf=False)
+
+    if not token or not payload:
+        return RedirectResponse('/login', status_code=303)
+
+    print(payload.sub)
+
     with Session(engine) as session:
         courses = session.exec(select(Course)).all()
         payments = session.exec(select(Payment)).all()
@@ -78,7 +94,11 @@ def login_form(
         statement = select(User).where(User.username == username).where(User.password == password)
         result = session.exec(statement).all()
 
-        if not result:
-            return RedirectResponse('/login', status_code=303)
+    if not result:
+        return RedirectResponse('/login', status_code=303)
 
-        return RedirectResponse('/', status_code=303)
+    token = auth.create_access_token(uid=result[0].username)
+    response = RedirectResponse('/', status_code=303)
+    auth.set_access_cookies(token, response)
+
+    return response
