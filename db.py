11c8c392d8d54c45834de1a172d96991397f93da
Erik5551 <Esargsan81@gmail.com>
2026-03-20 17:45:40 +0300

    first

--- db.py
+++ db.py
@@ -7,3 +7,29 @@ engine = create_engine(r'sqlite:///db.sqlite')
 class Course(SQLModel, table=True):
     id: int | None = Field(primary_key=True)
     title: str = Field(unique=True)
+
+
+class Payment(SQLModel, table=True):
+    id: int |  None = Field(primary_key=True)
+    title: str = Field(unique=True)
+
+
+class User(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    username: str = Field(unique=True)
+    password: str
+    first_name: str
+    last_name: str
+    second_name: str
+    phone: str
+    email: str
+    role: str | None = Field(default="user.id")
+
+
+
+class Request(SQLModel, table=True):
+    id: int | None = Field(primary_key=True)
+    date: str
+    payment_id: int = Field(foreign_key="payment.id")
+    course_id: int = Field(foreign_key="course.id")
+    user_id: int = Field(foreign_key="user.id")
