587d4d2a0afd404f9b0b9e2c5db304d27cc04af8
Трофимов Алексей <a.trofimov@domain.local>
2026-04-22 17:48:47 +0300

    add enums

--- admin/admin/models.py
+++ admin/admin/models.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from django.db import models
 
 
+class Payment(models.TextChoices):
+    CASH = "Наличные", "Наличные"
+    PHONE = "Перевод по номеру", "Перевод по номеру"
+
+
+class Status(models.TextChoices):
+    NEW = "Новая", "Новая"
+    PROGRESS = "Идёт обучение", "Идёт обучение"
+    COMPLETED = "Завершён", "Завершён"
+
+
 class User(models.Model):
     login = models.CharField()
     password = models.CharField()
@@ -17,8 +28,8 @@ class User(models.Model):
 class Record(models.Model):
     course = models.CharField()
     date = models.DateField()
-    payment = models.CharField()
-    status = models.CharField()
+    payment = models.CharField(choices=Payment)
+    status = models.CharField(choices=Status, default=Status.NEW)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
@@ -31,6 +42,7 @@ class RecordAdmin(admin.ModelAdmin):
     list_display = ["course", "date", "user__fio", "user__phone", "status"]
     list_filter = ["course", "status", "date"]
     search_fields = ["user__email"]
+
 
 admin.site.site_title  = "Админпанель"
 admin.site.site_header  = "Корки.Есть. Админпанель"
